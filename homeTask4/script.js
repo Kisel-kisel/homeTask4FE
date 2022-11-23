3ac4a106b644c9e32be67b842585c37cdb82c102
Vlad Kisialeuski <75371184+Kisel-kisel@users.noreply.github.com>
2022-11-23 20:12:18 +0100

    Add files via upload

---
+++ homeTask4/script.js
@@ -0,0 +1,17 @@
+// Задание 1
+
+// let age = prompt("What is your age?");
+// alert( `You are ${age} years old!`)
+
+// Задание2
+
+// let number = prompt("Write number!");
+// alert(number*number);
+
+// Задание3
+
+let numone = Number(prompt("Write first number"));
+let numtwo = Number(prompt("Write second number"));
+let numthree = numone+numtwo;
+alert(numthree);
+
